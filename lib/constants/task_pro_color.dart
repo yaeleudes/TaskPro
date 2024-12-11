35ab3cc7a73246e0c98ccbba060e2835b6e4ee86
eke333 <ehoumanjoel.fr@gmail.com>
2024-12-11 15:12:42 +0000

    premier merge

--- lib/constants/task_pro_color.dart
+++ lib/constants/task_pro_color.dart
@@ -4,4 +4,9 @@ class TaskProColor {
   static Color primary = const Color(0xFF0B2F9F);
   static Color secondary = const Color(0xFFF4A261);
   static Color third = const Color(0xFFD1D3D9);
+
+  static Color red = const Color(0xFFFF2929);
+  static Color green = const Color(0xFF06D001);
+  static Color yellow = const Color(0xFFFFB200);
+  static Color blue = const Color(0xFF3D3BF3);
 }
