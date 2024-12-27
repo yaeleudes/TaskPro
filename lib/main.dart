54a9e569781aa467ecd206797092ca5056d59896
eke333 <ehoumanjoel.fr@gmail.com>
2024-12-27 22:10:34 +0000

    Mise à jour du système de routage

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:task_pro/constants/task_pro_theme.dart';
-import 'package:task_pro/widgets/buttons/task_pro_floating_button.dart';
-import 'package:task_pro/widgets/modals/task_pro_modal.dart';
-
-import 'widgets/bottomNavbar/task_pro_bottom_navbar.dart';
-import 'widgets/buttons/task_pro_action_button.dart';
-import 'widgets/inputs/task_pro_common_input.dart';
-import 'widgets/inputs/task_pro_password_input.dart';
-import 'widgets/inputs/task_pro_research_input.dart';
-
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:task_pro/routes/routes.dart';
 
 void main() {
   initializeDateFormatting('fr_FR', null).then((_) {
@@ -25,11 +17,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      routerConfig: Routes.router,
+      title: 'Task Pro',
       theme: TaskProTheme.light,
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
       locale: const Locale('fr', 'FR'),
       localizationsDelegates: const [
         GlobalMaterialLocalizations.delegate,
@@ -41,99 +33,5 @@ class MyApp extends StatelessWidget {
         Locale('fr', 'FR'),
       ],
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  TextEditingController emailController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
-  TextEditingController searchController = TextEditingController();
-  int _counter = 0;
-  int _currentIndex = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  // final List<Widget> _pages = const [
-  //   TodayPage(),
-  //   AgendaPage(),
-  //   RecherchePage(),
-  //   ParcourirPage()
-  // ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _currentIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text(widget.title),
-        ),
-        body: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(10),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const Text(
-                  'You have pushed the button this many times:',
-                ),
-                Text(
-                  '$_counter',
-                  style: Theme.of(context).textTheme.headlineMedium,
-                ),
-                TaskProActionButton(
-                    buttonTitle: "Rejoindre TaskPro",
-                    onPressed: () {
-                      print("action");
-                    }),
-                SizedBox(
-                  height: 10,
-                ),
-                TaskProCommonInput(
-                    controller: emailController, hintText: "Votre e-mail"),
-                SizedBox(
-                  height: 10,
-                ),
-                TaskProPasswordInput(
-                  controller: passwordController,
-                  hintText: "Votre mot de passe",
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                TaskProResearchInput(
-                    controller: searchController,
-                    hintText: "tâches, catégorie, date et plus")
-              ],
-            ),
-          ),
-        ),
-
-        // body: _pages[_currentIndex],
-
-        bottomNavigationBar: TaskProBottomNavbar(
-          currentIndex: _currentIndex,
-          onTap: _onItemTapped,
-        ),
-        floatingActionButton: TaskProFloatingButton(onPressed: () {
-          TaskProModal.ajouterTache(context);
-        }));
   }
 }
--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:task_pro/screens/home_screen.dart';
+import 'package:task_pro/screens/pages/authentification/forgot_password.dart';
+import 'package:task_pro/screens/pages/authentification/login.dart';
+import 'package:task_pro/screens/pages/welcome/welcome_page.dart';
+
+import '../screens/pages/authentification/register.dart';
 
 class Routes {
   static final GlobalKey<NavigatorState> _rootNavigatorKey = GlobalKey<NavigatorState>(debugLabel: 'root');
@@ -12,9 +17,24 @@ class Routes {
     routes: [
       GoRoute(
         path: '/',
+        builder: (context, state) => WelcomePage(),
+      ),
+      GoRoute(
+        path: '/home',
         builder: (context, state) => HomeScreen(),
       ),
-      // On ajoute les autres routes à la suite.
+      GoRoute(
+        path: '/login',
+        builder: (context, state) => Login(),
+      ),
+      GoRoute(
+        path: '/register',
+        builder: (context, state) => Register(),
+      ),
+      GoRoute(
+        path: '/forgot_password',
+        builder: (context, state) => ForgotPassword(),
+      ),
     ],
 
     errorBuilder: (context, state) {
